55ca1497db23c5c06ebd65acc89a764b37b57194
vanessa.gyger <vanessa.gyger@ost.ch>
2022-12-15 11:43:47 +0100

    added find

--- wuff-find.py
+++ wuff-find.py
@@ -1,40 +1,40 @@
+import click
 import requests
 import csv
 import argparse
+import datetime
+
+
 
 def fetchData():
     res = requests.get("https://data.stadt-zuerich.ch/dataset/sid_stapo_hundenamen_od1002/download/KUL100OD1002.csv")
+    if not res:
+        return
     res.encoding = "utf-8-sig"
     return res.text.splitlines()
 
-def find(year):
+
+def find(name, year):
     file = csv.DictReader(fetchData())
+    print(f'Year: {year} with name: {name}')
+    foundOne = False
     for row in file:
+        if row["StichtagDatJahr"] == str(year) and row["HundenameText"] == name:
+            birthdate = 'w' if row["SexHundLang"] == 'weiblich' else 'm'
+            print(f'{name} {row["GebDatHundJahr"]}, {birthdate}')
+            foundOne = True
+    if not foundOne:
+        return click.echo(f'No dog found in the database with the name \'{name}\' and the year \'{year}\'')
 
-        if row["StichtagDatJahr"] == year and row["HundenameText"] == "Adonis":
-
-            print(row)
-
-def run(args):
-    if args.year:
-        find(args.year)
-    else:
-        find('2022')
-
-def get_parser():
-    parser = argparse.ArgumentParser()
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument(
-        '--year',
-        dest = 'year',
-        type = str
-    )
-
-    return parser
-
-def main(args=None):
-    parsed = get_parser().parse_args(args)
-    run(parsed)
+@click.argument('name', type=str)
+@click.option('-y', '--year',  type=int, help='The year to search for your dog. Not birthdate and min 2015')
+@click.command()
+def run(name, year):
+    if year and len(str(year)) != 4:
+       return click.echo('Error: Invalid year. Please provide a four digit number.')
+    elif not year:
+        year = datetime.date.today().year
+    find(name, year)
 
 if __name__ == '__main__':
-    main()
+    run()
